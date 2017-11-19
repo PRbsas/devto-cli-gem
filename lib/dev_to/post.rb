74d8e413909bb24ed63945f5c7d0729e64c04211
PRbsas <paularamirez0316@gmail.com>
2017-11-19 14:01:42 -0500

    Mass assign Post attr from hash, modify scraper method to return hash

--- lib/dev_to/post.rb
+++ lib/dev_to/post.rb
@@ -3,7 +3,8 @@ class DevTo::Post
 
   @@all = []
 
-  def initialize
+  def initialize(attributes_hash)
+    attributes_hash.each {|attribute, value| self.send(("#{attribute}="), value)}
     self.class.all << self
   end
 
--- lib/dev_to/scraper.rb
+++ lib/dev_to/scraper.rb
@@ -4,26 +4,30 @@ class DevTo::Scraper
     Nokogiri::HTML(open("https://dev.to/"))
   end
 
-  def get_posts
-    self.get_page.css(".articles-list .substories .single-article")
+  def posts
+    posts ||= get_page.css(".articles-list .substories .single-article")
+  end
+
+  def scrape_posts(post)
+    {
+      :title => post.search(".content h3").text.strip.gsub(/[[:space:]]+/," "),
+      :author => post.search("h4").text.strip,
+      :tags => post.search(".tags .tag").collect { |tag| tag.text.strip},
+      :url => "https://dev.to" + post.search("a").last.attr("href"),
+      :comments => post.search(".engagement-count-number").first.text.strip,
+      :likes => post.search(".engagement-count-number").last.text.strip,
+    }
   end
 
   def make_posts
-    self.get_posts.each do |data|
-      post = DevTo::Post.new
-      post.title = data.search(".content h3 span").remove
-      post.title = data.search(".content h3").text.strip.gsub(/[[:space:]]+/," ")
-      post.author = data.search("h4").text.strip
-      post.tags = data.search(".tags .tag").collect { |tag| tag.text.strip}
-      post.url = "https://dev.to" + data.search("a").last.attr("href")
-      post.comments = data.search(".engagement-count-number").first.text.strip
-      post.likes = data.search(".engagement-count-number").last.text.strip
+    posts.each do |post|
+      DevTo::Post.new(scrape_posts(post))
     end
   end
 
-  def make_content(current_post)
-    post_page = Nokogiri::HTML(open("#{current_post.url}"))
-    current_post.content = post_page.search("body #article-body").text.strip
-    current_post.date = post_page.search("#main-title .published-at").text.strip
-  end
+  #def make_content(current_post)
+  #  post_page = Nokogiri::HTML(open("#{current_post.url}"))
+  #  current_post.content = post_page.search("body #article-body").text.strip
+  #  current_post.date = post_page.search("#main-title .published-at").text.strip
+  #end
 end
