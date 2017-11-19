3f0dd437952a883a3ada4ac5b16269315516063a
PRbsas <paularamirez0316@gmail.com>
2017-11-19 17:24:10 -0500

    Require bundler and use require_all in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
+require 'bundler'
+Bundler.require
+
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
 require 'colorize'
 require 'require_all'
 
-require_relative '../lib/dev_to/version'
-require_relative '../lib/dev_to/cli'
-require_relative '../lib/dev_to/post'
-require_relative '../lib/dev_to/scraper'
+require_all 'lib'
